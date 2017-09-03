e4b7ddb79fb7e628a60420b5826b7b3000f395fd
Jean Thome <jtc.jean@gmail.com>
2017-09-03 17:24:53 -0300

    Criado controller para obter lista de estádios.

--- apicore/src/main/java/brasileirao/api/controller/MatchController.java
+++ apicore/src/main/java/brasileirao/api/controller/MatchController.java
@@ -1,15 +1,21 @@
 package brasileirao.api.controller;
 
 import brasileirao.api.dto.MatchDto;
+import brasileirao.api.enums.StadiumEnum;
 import brasileirao.api.persistence.Match;
 import brasileirao.api.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Lida com as requisicões referentes à entidade {@link Match}
@@ -42,6 +48,24 @@ public class MatchController {
       } else {
          return new ResponseEntity<>("Não encontrado", HttpStatus.NOT_FOUND);
       }
+   }
 
+   /**
+    * Obtém a lista dos estádios onde os jogos podem ser realizados.
+    *
+    * @return Lista com as informações do estádios em StadiumEnum.
+    */
+   @GetMapping(value = "/stadiums", produces = MediaType.APPLICATION_JSON_VALUE)
+   public ResponseEntity<?> getStadiums() {
+
+      final List<HashMap<String, String>> stadiumEnumList = new ArrayList<>();
+
+      for (StadiumEnum stadiumEnum : StadiumEnum.values()) {
+         final HashMap<String, String> hashMap = new HashMap<>();
+         hashMap.put("value", stadiumEnum.name());
+         hashMap.put("label", stadiumEnum.getName());
+         stadiumEnumList.add(hashMap);
+      }
+      return new ResponseEntity<Object>(stadiumEnumList, HttpStatus.OK);
    }
 }
