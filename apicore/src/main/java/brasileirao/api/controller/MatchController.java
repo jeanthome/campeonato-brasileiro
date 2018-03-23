708310f641bcfc2cc93339a47cc92295ee96202a
Jean Thome <jtc.jean@gmail.com>
2018-03-23 18:03:42 -0300

    Remoção de método auxiliar em MatchController -Removido o método usado como facilitador de cadastro de partidas.

--- apicore/src/main/java/brasileirao/api/controller/MatchController.java
+++ apicore/src/main/java/brasileirao/api/controller/MatchController.java
@@ -1,11 +1,9 @@
 package brasileirao.api.controller;
 
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import brasileirao.api.helper.ConverterHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -32,6 +30,7 @@ import brasileirao.api.enums.StadiumEnum;
 import brasileirao.api.enums.ValidationExceptionMessageEnum;
 import brasileirao.api.exception.ServiceException;
 import brasileirao.api.exception.ValidationException;
+import brasileirao.api.helper.ConverterHelper;
 import brasileirao.api.helper.ValidationHelper;
 import brasileirao.api.persistence.Match;
 import brasileirao.api.service.MatchService;
@@ -65,7 +64,7 @@ public class MatchController {
     if (!ValidationHelper.isNumber(id)) {
       throw new ValidationException(ValidationExceptionMessageEnum.INVALID_NUMBER.getMessage());
     }
-    
+
     final MatchDto matchDto = this.matchService.findById(ConverterHelper.convertStringToLong(id));
     return new ResponseEntity<Object>(matchDto, HttpStatus.OK);
   }
@@ -89,77 +88,9 @@ public class MatchController {
     if (result.hasErrors()) {
       return new ResponseEntity<>("Dados incorretos", HttpStatus.BAD_REQUEST);
     } else {
-
-      // this.insertMatches();
       this.matchService.insertMatch(matchInputDto);
     }
     return new ResponseEntity<Object>("Partida inserida", HttpStatus.OK);
-  }
-
-  // TODO: ESTE MÉTODO DEVE SER REMOVIDO. USADO SOMENTE COMO FACILITADOR PARA INSERÇÃO DE PARTIDAS.
-  @GetMapping(value = "/insert", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Object> insertMatches() throws ParseException, ServiceException {
-
-    final HashMap<String, Long> clubs = new HashMap<>();
-    clubs.put("ACG", 41L);
-    clubs.put("CAM", 42L);
-    clubs.put("CAP", 43L);
-    clubs.put("AVA", 44L);
-    clubs.put("BAH", 45L);
-    clubs.put("BOT", 46L);
-    clubs.put("CHA", 47L);
-    clubs.put("COR", 48L);
-    clubs.put("CFC", 49L);
-    clubs.put("CRU", 50L);
-    clubs.put("FLA", 51L);
-    clubs.put("FLU", 52L);
-    clubs.put("GRE", 53L);
-    clubs.put("PAL", 54L);
-    clubs.put("PON", 55L);
-    clubs.put("SAN", 56L);
-    clubs.put("SAO", 57L);
-    clubs.put("SPO", 58L);
-    clubs.put("VAS", 59L);
-    clubs.put("VIT", 60L);
-
-    final List<MatchInputDto> dtos = new ArrayList<>();
-
-    final Long round = 28L;
-    dtos.add(new MatchInputDto(round, "07/10/2017 16:00", StadiumEnum.MINEIRAO, clubs.get("CRU"),
-        clubs.get("PON")));
-
-    dtos.add(new MatchInputDto(round, "14/10/2017 19:00", StadiumEnum.MARACANA, clubs.get("VAS"),
-        clubs.get("BOT")));
-
-    dtos.add(new MatchInputDto(round, "14/10/2017 21:00", StadiumEnum.PACAEMBU, clubs.get("SAO"),
-        clubs.get("CAP")));
-
-    dtos.add(new MatchInputDto(round, "15/10/2017 17:00", StadiumEnum.MARACANA, clubs.get("FLU"),
-        clubs.get("AVA")));
-
-    dtos.add(new MatchInputDto(round, "15/10/2017 17:00", StadiumEnum.ILHA_DO_RETIRO, clubs
-        .get("SPO"), clubs.get("CAM")));
-
-    dtos.add(new MatchInputDto(round, "15/10/2017 17:00", StadiumEnum.ESTADIO_OLIMPICO, clubs
-        .get("ACG"), clubs.get("PAL")));
-
-    dtos.add(new MatchInputDto(round, "15/10/2017 17:00", StadiumEnum.ARENA_CONDA,
-        clubs.get("CHA"), clubs.get("FLA")));
-
-    dtos.add(new MatchInputDto(round, "15/10/2017 19:00", StadiumEnum.COUTO_PEREIRA, clubs
-        .get("CFC"), clubs.get("GRE")));
-
-    dtos.add(new MatchInputDto(round, "15/10/2017 19:00", StadiumEnum.FONTE_NOVA, clubs.get("BAH"),
-        clubs.get("COR")));
-
-    dtos.add(new MatchInputDto(round, "16/10/2017 20:00", StadiumEnum.PACAEMBU, clubs.get("SAN"),
-        clubs.get("VIT")));
-
-    for (MatchInputDto dto : dtos) {
-      this.matchService.insertMatch(dto);
-    }
-
-    return new ResponseEntity<Object>("Inserido", HttpStatus.OK);
   }
 
   /**
